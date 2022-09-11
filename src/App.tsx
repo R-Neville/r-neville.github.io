62d8620741315a3c5f75236a4b541b5c48e55dc8
Robert Neville <robertneville27@gmail.com>
2022-09-11 10:14:27 +1000

    Start work on contact section. Create Expander for progressively showing content in sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { addEvent, removeEvent } from "./events";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Footer from "./components/Footer";
+import GitSrc from "./assets/images/github-8-64.png";
+import LinkedInSrc from "./assets/images/linkedin-3-64.png";
+import Expander from "./components/Expander";
 
 export interface LinkItem {
   text: string;
@@ -18,6 +21,8 @@ const SKILLS_SECTION_ID = "skills";
 const PROJECTS_SECTION_ID = "projects";
 const CONTACT_SECTION_ID = "contact";
 const MAX_MENU_WIDTH = 920;
+const MY_GITHUB = "https://github.com/R-Neville";
+const MY_LINKEDIN = "www.linkedin.com/in/r-neville";
 
 function App() {
   let theme = themes.dark;
@@ -159,12 +164,29 @@ function App() {
       <Section
         id={TOP_SECTION_ID}
         heading={"👋 Hi!"}
-        children={
-          <p style={pStyles}>
+        children={[
+          <p key={1} style={pStyles}>
             My name is Robbie. I'm a junior JavaScript and TypeScript developer,
             and this is my portfolio site!
-          </p>
-        }
+          </p>,
+          <Expander
+            key={2}
+            children={[
+              <p key={1} style={{ display: "none", ...pStyles }}>
+                I'm a passionate programmer and I've use numerous languages in
+                the past, including C, C++, and Python, but I am particularly
+                drawn to Web development.
+              </p>,
+              <p key={2} style={{ display: "none", ...pStyles }}>
+                I'm familiar with Node.js - I've worked on a handful of personal
+                Express.js websites - and I'm currently learning Ruby on Rails.
+              </p>,
+              <p key={3} style={{ display: "none", ...pStyles }}>
+                I'm also into Electron.js and I'm building my own IDE!
+              </p>,
+            ]}
+          />,
+        ]}
       />
     );
   };
@@ -177,8 +199,8 @@ function App() {
         children={[
           <p key={1} style={pStyles}>
             I was born in Australia, and grew up in rural Queensland. I
-            discovered programming after I finished highschool, and completed
-            half a Bachelor of IT at The University of Southern Queensland,
+            discovered programming after I finished highschool, and studied
+            IT at the University of Southern Queensland for two years
             before enrolling in a Web development bootcamp at Coder Academy. I'm
             due to finish in November, 2022, and can't wait to kickstart my
             career in the industry!
@@ -209,7 +231,26 @@ function App() {
   };
 
   const buildContactSection = () => {
-    return <Section id={CONTACT_SECTION_ID} heading={"📥 Contact"} />;
+    return (
+      <Section
+        id={CONTACT_SECTION_ID}
+        heading={"📥 Contact"}
+        children={[
+          <p key={1} style={pStyles}>
+            If you would like to contact me about work or collaboration, please
+            don't hesitate to connect with me on LinkedIn!
+          </p>,
+          <div key={2}>
+            <a href={MY_LINKEDIN} style={{ margin: "0.5em" }}>
+              <img src={LinkedInSrc} alt="LinkedIn"></img>
+            </a>
+            <a href={MY_GITHUB} style={{ margin: "0.5em" }}>
+              <img src={GitSrc} alt="GitHub"></img>
+            </a>
+          </div>,
+        ]}
+      />
+    );
   };
 
   return (
---
+++ src/components/Expander.tsx
@@ -0,0 +1,111 @@
+import { MouseEvent, useCallback, useState } from "react";
+import { slideDown, slideUp } from "../helpers";
+import themes from "../themes";
+
+const ANIMATION_DURATION_FACTOR = 10;
+
+function Expander(props: { children: JSX.Element | JSX.Element[] }) {
+  const theme = themes.dark;
+
+  const btnStyles = {
+    padding: "0.5em 1em",
+    border: "none",
+    backgroundColor: theme.bgPrimary,
+    fontSize: "1em",
+    color: theme.fgPrimary,
+    cursor: "pointer",
+  };
+
+  const [childIndex, setChildIndex] = useState(0);
+
+  const onShowMoreButtonClick = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
+      const button = event.target as HTMLElement;
+      const parent = button.closest(".expander");
+      const expandables = parent?.querySelector(".expandables")?.children;
+      if (parent && expandables) {
+        const expandablesArray = Array.from(expandables);
+        if (childIndex < expandablesArray.length) {
+          setChildIndex(childIndex + 1);
+          slideDown(
+            expandablesArray[childIndex] as HTMLElement,
+            "block",
+            ANIMATION_DURATION_FACTOR
+          );
+          if (childIndex === expandablesArray.length - 1) {
+            button.style.display = "none";
+            const hideButton = parent.querySelector(".hide") as HTMLElement;
+            if (hideButton) {
+              hideButton.style.display = "block";
+            }
+          }
+        }
+      }
+    },
+    [childIndex]
+  );
+
+  const onHideButtonClick = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
+      const button = event.target as HTMLElement;
+      const parent = button.closest(".expander") as HTMLElement;
+      const expandables = parent?.querySelector(".expandables") as HTMLElement;
+      if (parent && expandables) {
+        setChildIndex(0);
+        button.style.display = "none";
+        slideUp(expandables, ANIMATION_DURATION_FACTOR, () => {
+          Array.from(expandables.children).forEach((e) => {
+            (e as HTMLElement).style.display = "none";
+          });
+          expandables.style.display = "block";
+          const showMoreButton = parent.querySelector(
+            ".show-more"
+          ) as HTMLElement;
+          if (showMoreButton) {
+            showMoreButton.style.display = "block";
+          }
+        });
+      }
+    },
+    []
+  );
+
+  function onBtnMouseEnter(event: MouseEvent<HTMLButtonElement>) {
+    const button = event.target as HTMLElement;
+    button.style.backgroundColor = theme.bgAccent;
+  }
+
+  function onBtnMouseLeave(event: MouseEvent<HTMLButtonElement>) {
+    const button = event.target as HTMLElement;
+    button.style.backgroundColor = theme.bgPrimary;
+  }
+
+  return (
+    <div
+      className="expander"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <div className="expandables">{props.children}</div>
+      <button
+        className="show-more"
+        style={btnStyles}
+        onClick={onShowMoreButtonClick}
+        onMouseEnter={onBtnMouseEnter}
+        onMouseLeave={onBtnMouseLeave}
+      >
+        Show More
+      </button>
+      <button
+        className="hide"
+        style={{ display: "none", ...btnStyles }}
+        onClick={onHideButtonClick}
+        onMouseEnter={onBtnMouseEnter}
+        onMouseLeave={onBtnMouseLeave}
+      >
+        Hide
+      </button>
+    </div>
+  );
+}
+
+export default Expander;
