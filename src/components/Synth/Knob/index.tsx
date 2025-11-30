eab6e69463dc05de482d0c96d0877133360ae566
Robert Neville <robertneville27@gmail.com>
2025-12-01 07:49:43 +1000

    Add support for touch events to Knob component.

--- src/components/Synth/Knob/index.tsx
+++ src/components/Synth/Knob/index.tsx
@@ -1,5 +1,10 @@
 import arePropsEqual from '#/utils/arePropsEqual'
-import React, { FC, useState } from 'react'
+import React, {
+    FC,
+    MouseEvent as ReactMouseEvent,
+    TouchEvent as ReactTouchEvent,
+    useState,
+} from 'react'
 
 interface KnobProps {
     numberOfSteps: number
@@ -20,6 +25,54 @@ const KnobComponent: FC<KnobProps> = ({
 }) => {
     const [rotation, setRotation] = useState(value)
 
+    const onMouseDown = (event: ReactMouseEvent<HTMLDivElement>) => {
+        const startX = event.clientX
+
+        const controller = new AbortController()
+
+        const handleMouseMove = (event: MouseEvent) => {
+            const deltaX = event.clientX - startX
+            const newValue = Math.min(
+                Math.max(rotation + deltaX, 0),
+                maxRotation,
+            )
+            const step = Math.round((newValue / maxRotation) * numberOfSteps)
+            onChange(step)
+            setRotation(newValue)
+        }
+
+        const handleMouseUp = () => {
+            controller.abort()
+        }
+
+        window.addEventListener('mousemove', handleMouseMove, controller)
+        window.addEventListener('mouseup', handleMouseUp, controller)
+    }
+
+    const onTouchStart = (event: ReactTouchEvent<HTMLDivElement>) => {
+        const startX = event.touches[0].clientX
+
+        const controller = new AbortController()
+
+        const handleTouchMove = (event: TouchEvent) => {
+            const deltaX = event.touches[0].clientX - startX
+            const newValue = Math.min(
+                Math.max(rotation + deltaX, 0),
+                maxRotation,
+            )
+            const step = Math.round((newValue / maxRotation) * numberOfSteps)
+            onChange(step)
+            setRotation(newValue)
+        }
+
+        const handleTouchEnd = () => {
+            controller.abort()
+        }
+
+        window.addEventListener('touchmove', handleTouchMove, controller)
+        window.addEventListener('touchend', handleTouchEnd, controller)
+    }
+
     return (
         <div className="flex flex-col items-center bg-primary-800 gap-2 p-2 select-none rounded border border-primary-600">
             <div className="text-primary-200">{label}</div>
@@ -29,39 +82,8 @@ const KnobComponent: FC<KnobProps> = ({
                     style={{
                         transform: `rotate(${rotation}deg)`,
                     }}
-                    onMouseDown={(event) => {
-                        const startX = event.clientX
-
-                        const controller = new AbortController()
-
-                        const handleMouseMove = (event: MouseEvent) => {
-                            const deltaX = event.clientX - startX
-                            const newValue = Math.min(
-                                Math.max(rotation + deltaX, 0),
-                                maxRotation,
-                            )
-                            const step = Math.round(
-                                (newValue / maxRotation) * numberOfSteps,
-                            )
-                            onChange(step)
-                            setRotation(newValue)
-                        }
-
-                        const handleMouseUp = () => {
-                            controller.abort()
-                        }
-
-                        window.addEventListener(
-                            'mousemove',
-                            handleMouseMove,
-                            controller,
-                        )
-                        window.addEventListener(
-                            'mouseup',
-                            handleMouseUp,
-                            controller,
-                        )
-                    }}
+                    onMouseDown={onMouseDown}
+                    onTouchStart={onTouchStart}
                 >
                     <div className="absolute bottom-2 left-2 rounded-full h-1 w-1 bg-primary-800"></div>
                 </div>
